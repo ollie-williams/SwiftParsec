2c6922b05f9be773c4d90acd1b598fead79e51fd
Oliver Williams <ollie@olliewilliams.org>
2014-12-08 11:33:00 -0800

    Adding Pipe2

--- pipe.swift
+++ pipe.swift
@@ -22,3 +22,35 @@ class Pipe<T:Parser, V> : Parser {
     }
   }
 }
+
+
+class Pipe2<T1:Parser, T2:Parser, V> : Parser {
+  typealias TargetType = V
+  typealias R1 = T1.TargetType
+  typealias R2 = T2.TargetType
+
+  let first: T1
+  let second: T2
+  let fn: (R1, R2) -> V
+
+  init(first:T1, second:T2, fn: (R1, R2) -> V) {
+    self.first = first
+    self.second = second
+    self.fn = fn
+  }
+
+  func parse<S: CharStream>(stream: S) -> Result<TargetType, S> {
+    let fb = FollowedBy(first:first, second:second)
+    switch fb.parseBoth(stream) {
+      case .Success(let vals, let str):
+        let (a,b) = vals.item
+        return Result(
+          value : fn(a, b),
+          stream: str.item
+          )
+        case .Failure(let str, let msg):
+          return Result(stream:str.item, msg:msg)
+    }
+  }
+
+}
