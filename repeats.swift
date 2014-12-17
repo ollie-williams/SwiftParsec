b24971aa102f14b0b097a83fa7f632f9e1a5e8d6
Oliver Williams <ollie@olliewilliams.org>
2014-12-17 14:56:42 -0800

    Implementing sepby operator.

--- repeats.swift
+++ repeats.swift
@@ -28,3 +28,32 @@ func many<T:Parser>(body:T) -> Many<T> {
 func many1<T:Parser>(body:T) -> Many<T> {
   return Many(body:body, emptyOk:false)
 }
+
+class SepBy<T:Parser, S:Parser> : Parser {
+  typealias Target = [T.Target]
+
+  let item: T
+  let sep:  S
+  let pair: FollowedBySecond<S,T>
+
+  init(item:T, sep:S) {
+    self.item = item
+    self.sep = sep
+    self.pair = sep >~ item
+  }
+
+  func parse(stream:CharStream) -> Target? {
+    var result = Target()
+    if let x = item.parse(stream) {
+      result.append(x)
+      while let next = pair.parse(stream) {
+        result.append(next)
+      }
+    }
+    return result
+  }
+}
+
+func sepby<T:Parser, S:Parser>(item:T, sep:S) -> SepBy<T,S> {
+  return SepBy(item:item, sep:sep)
+}
