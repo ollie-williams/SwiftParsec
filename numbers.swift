4d21936e6bdf903f0e4f287088b024569de41798
Oliver Williams <ollie@olliewilliams.org>
2014-12-23 11:55:12 -0800

    More static FloatParser.

--- numbers.swift
+++ numbers.swift
@@ -13,17 +13,19 @@ class Integer : Parser {
   }
 }
 
-func stringToFloat(str:String) -> Double {
-  return (str as NSString).doubleValue
-}
-class FloatParser : Parser {
+
+struct FloatParser : Parser {
   typealias Target = Double
 
   private let strict:Bool
 
-  // class variables not yet supported
-  private /*class*/ let impl
-      = pipe(regex("[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?"), stringToFloat)
+  static func stringToFloat(str:String) -> Double {
+    return (str as NSString).doubleValue
+  }
+
+  static let impl = pipe(
+        regex("[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?"),
+        FloatParser.stringToFloat)
 
   init(strict:Bool) {
     self.strict = strict
@@ -31,21 +33,20 @@ class FloatParser : Parser {
 
   func parse(stream:CharStream) -> Target? {
     if !strict {
-      return impl.parse(stream)
+      return FloatParser.impl.parse(stream)
     }
 
     let start = stream.pos
     if let ip = Integer().parse(stream) {
       let intend = stream.pos
       stream.pos = start
-      if let fp = impl.parse(stream) {
+      if let fp = FloatParser.impl.parse(stream) {
         if stream.pos == intend {
           return nil
         }
         return fp
       }
     }
-    return impl.parse(stream)
+    return FloatParser.impl.parse(stream)
   }
-
 }
