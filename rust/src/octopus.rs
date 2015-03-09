165e4da768b5bcbd0704dcabbb0e0b6b0398f428
Oliver Williams <olliew@apple.com>
2015-03-09 16:44:36 -0700

    Matching a regular expression.

--- rust/src/octopus.rs
+++ rust/src/octopus.rs
@@ -1,3 +1,16 @@
+extern crate regex;
+use regex::Regex;
+
 fn main() {
-  println!("Hello world!\n")
+  let ipt = "Hello world!";
+  let rx = match Regex::new(r"^Hello") {
+      Ok(re) => re,
+      Err(err) => panic!("Error: {}", err)
+  };
+
+  if rx.is_match(ipt) {
+      println!("Match\n");
+  } else {
+      println!("No match\n");
+  }
 }
