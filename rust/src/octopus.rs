8069fa8e40766641c52fa9aba0edb052e5841b7c
Oliver Williams <olliew@apple.com>
2015-03-09 17:17:25 -0700

    Passing through function.

--- rust/src/octopus.rs
+++ rust/src/octopus.rs
@@ -1,21 +1,25 @@
 extern crate regex;
 use regex::Regex;
 
+fn find_hello(s:&str) -> Option<usize> {
+    let rx = match Regex::new(r"^Hello") {
+        Ok(re) => re,
+        Err(err) => panic!("Error: {}", err),
+    };
+
+    match rx.find(s) {
+        Some(uv) => return Some(uv.1),
+        None => return None
+    }
+}
+
 fn main() {
   let ipt = "Hello world!";
-  let rx = match Regex::new(r"^Hello") {
-      Ok(re) => re,
-      Err(err) => panic!("Error: {}", err)
-  };
 
-  let fnd = rx.find(ipt);
-  if let Some(uv) = fnd {
-      println!("{} {}", uv.0, uv.1);
-      let rem = &ipt[uv.1..];
+  if let Some(ind) = find_hello(ipt) {
+      let rem = &ipt[ind..];
       println!("Remainder: {}", rem);
   } else {
       println!("No match");
-      return;
   }
-
 }
